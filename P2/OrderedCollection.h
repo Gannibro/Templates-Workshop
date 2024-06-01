ac8a86e6c452e40346f9a457aaeea52590118282
Aryaman Giri <aryamankgiri@gmail.com>
2024-06-01 15:51:32 +0545

    Separated .h and .cpp files

--- P2/OrderedCollection.h
+++ P2/OrderedCollection.h
@@ -8,25 +8,8 @@ namespace seneca {
 template <typename T>
 class OrderedCollection : public Collection<T, 72> {
 public:
-    bool operator+=(const T& item) {
-        if (this->size() < this->capacity()) {
-            int i = this->size() - 1;
-            while (i >= 0 && this->operator[](i) > item) {
-                this->operator[](i + 1) = this->operator[](i);
-                --i;
-            }
-            this->operator[](i + 1) = item;
-            this->incrSize();
-            this->setSmallestItem(item);
-            this->setLargestItem(item);
-            return true;
-        }
-        return false;
-    }
-
+    bool operator+=(const T& item);
 };
-
-
 
 } // namespace seneca
 
