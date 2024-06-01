25ea56bb8d752f92df2cf38572cf9d02d1862bfa
Aryaman Giri <aryamankgiri@gmail.com>
2024-06-01 09:17:52 +0545

    Updated Part-II

--- P2/OrderedCollection.h
+++ P2/OrderedCollection.h
@@ -8,9 +8,26 @@ namespace seneca {
 template <typename T>
 class OrderedCollection : public Collection<T, 72> {
 public:
-    bool operator+=(const T& item);
+    bool operator+=(const T& item) {
+        if (this->size() < this->capacity()) {
+            int i = this->size() - 1;
+            while (i >= 0 && this->operator[](i) > item) {
+                this->operator[](i + 1) = this->operator[](i);
+                --i;
+            }
+            this->operator[](i + 1) = item;
+            this->incrSize();
+            this->setSmallestItem(item);
+            this->setLargestItem(item);
+            return true;
+        }
+        return false;
+    }
+
 };
+
+
 
 } // namespace seneca
 
-#endif // SENECA_ORDEREDCOLLECTION_H
+#endif
