6c48b6df2340324a3b3b3ffbafd8f22d2e64f3db
Aryaman Giri <aryamankgiri@gmail.com>
2024-06-03 06:11:39 +0545

    Revised Part-I

--- P1/Collection.h
+++ P1/Collection.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
+#include <limits>
 #include "Book.h"
 
 namespace seneca {
@@ -13,15 +15,46 @@ class Collection {
     size_t count;
 
 public:
-    Collection();
+    Collection() : count(0) {}
 
-    bool add(const T& item);
-    size_t size() const;
-    size_t capacity() const;
-    T getSmallestItem() const;
-    T getLargestItem() const;
-    Collection& operator+=(const T& item);
-    void print(std::ostream& os) const;
+    bool add(const T& item) {
+        if (count < Capacity) {
+            items[count++] = item;
+            return true;
+        }
+        return false;
+    }
+
+    size_t size() const {
+        return count;
+    }
+
+    size_t capacity() const {
+        return Capacity;
+    }
+
+    T getSmallestItem() const {
+        return *std::min_element(items, items + count);
+    }
+
+    T getLargestItem() const {
+        return *std::max_element(items, items + count);
+    }
+
+    Collection& operator+=(const T& item) {
+        add(item);
+        return *this;
+    }
+
+    void print(std::ostream& os) const {
+        os << "[";
+        for (size_t i = 0; i < count; ++i) {
+            os << items[i];
+            if (i != count - 1) os << ",";
+        }
+        os << "]";
+        std::cout << std::endl;
+    }
 };
 
 // Specialization for Book
@@ -31,19 +64,69 @@ class Collection<Book, 10> {
     size_t count;
 
 public:
-    Collection();
+    Collection() : count(0) {}
 
-    bool add(const Book& item);
-    size_t size() const;
-    size_t capacity() const;
-    Book getSmallestItem() const;
-    Book getLargestItem() const;
-    Collection& operator+=(const Book& item);
-    void print(std::ostream& os) const;
+    bool add(const Book& item) {
+        if (count < 10) {
+            items[count++] = item;
+            return true;
+        }
+        return false;
+    }
+
+    size_t size() const {
+        return count;
+    }
+
+    size_t capacity() const {
+        return 10;
+    }
+
+    Book getSmallestItem() const {
+        auto min_ratio = std::numeric_limits<double>::max();
+        Book smallest_book;
+        for (size_t i = 0; i < count; ++i) {
+            double ratio = items[i].pagesToChaptersRatio();
+            if (ratio < min_ratio) {
+                min_ratio = ratio;
+                smallest_book = items[i];
+            }
+        }
+        return smallest_book;
+    }
+
+    Book getLargestItem() const {
+        auto max_ratio = std::numeric_limits<double>::min();
+        Book largest_book;
+        for (size_t i = 0; i < count; ++i) {
+            double ratio = items[i].pagesToChaptersRatio();
+            if (ratio > max_ratio) {
+                max_ratio = ratio;
+                largest_book = items[i];
+            }
+        }
+        return largest_book;
+    }
+
+    Collection& operator+=(const Book& item) {
+        add(item);
+        return *this;
+    }
+
+    void print(std::ostream& os) const {
+        os << "[After creating collection in empty state]" << std::endl;
+        os << "Book with smallest pages-to-chapters ratio (initial-default state): " << std::endl;
+        os << getSmallestItem() << std::endl;
+        os << "Book with largest pages-to-chapters ratio (initial-default state): " << std::endl;
+        os << getLargestItem() << std::endl;
+        os << "size/capacity: " << count << "/" << 10 << std::endl;
+    }
 };
 
-}
+// Explicit template instantiation
+template class Collection<int, 10>;
+template class Collection<double, 10>;
 
-#include "Collection.cpp"
+}
 
 #endif // COLLECTION_H
