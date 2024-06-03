4551787ec47898bf47d0abcadea97336ec577275
Aryaman Giri <aryamankgiri@gmail.com>
2024-06-03 06:11:53 +0545

    Revised Part-II

--- P2/Collection.h
+++ P2/Collection.h
@@ -16,28 +16,69 @@ class Collection {
     static T m_largestItem;
 
 protected:
-    T& operator[](unsigned index);
+    T& operator[](unsigned index) {
+        return m_items[index];
+    }
 
-    void incrSize();
+    void incrSize() {
+        if (m_size < C) {
+            ++m_size;
+        }
+    }
 
-    void setSmallestItem(const T& item);
+    void setSmallestItem(const T& item) {
+        if (item < m_smallestItem || m_size == 0) {
+            m_smallestItem = item;
+        }
+    }
 
-    void setLargestItem(const T& item);
+    void setLargestItem(const T& item) {
+        if (item > m_largestItem || m_size == 0) {
+            m_largestItem = item;
+        }
+    }
 
 public:
-    Collection();
+    Collection() {}
 
-    unsigned size() const;
+    unsigned size() const {
+        return m_size;
+    }
 
-    unsigned capacity() const;
+    unsigned capacity() const {
+        return C;
+    }
 
-    static T getSmallestItem();
+    static T getSmallestItem() {
+        return m_smallestItem;
+    }
 
-    static T getLargestItem();
+    static T getLargestItem() {
+        return m_largestItem;
+    }
 
-    bool operator+=(const T& item);
+    bool operator+=(const T& item) {
+        if (m_size < C) {
+            m_items[m_size] = item;
+            setSmallestItem(item);
+            setLargestItem(item);
+            incrSize();
+            return true;
+        }
+        return false;
+    }
 
-    void print(std::ostream& os) const;
+    void print(std::ostream& os) const {
+        std::cout << std::endl;
+        std::cout << "[";
+        for (unsigned i = 0; i < m_size; ++i) {
+            os << m_items[i];
+            if (i < m_size - 1)
+                std::cout << ",";
+        }
+        std::cout << "]";
+        std::cout << std::endl;
+    }
 };
 
 // Static member declarations
@@ -46,6 +87,39 @@ T Collection<T, C>::m_smallestItem;
 
 template <typename T, unsigned int C>
 T Collection<T, C>::m_largestItem;
+
+// Specialized print implementation for Book
+template <>
+void Collection<seneca::Book, 10>::print(std::ostream& os) const {
+    os << "| --------------------------------------------------------------------------------------- |\n";
+    for (unsigned i = 0; i < m_size; ++i) {
+        os << "| ";
+        m_items[i].print(os);
+        os << "     |\n";
+    }
+    os << "| --------------------------------------------------------------------------------------- |";
+    std::cout << std::endl;
+}
+
+template <>
+void Collection<seneca::Book, 72>::print(std::ostream& os) const {
+    os << "| ------------------------------------------------------------------------------------- |\n";
+    for (unsigned i = 0; i < m_size; ++i) {
+        os << "| ";
+        m_items[i].print(os);
+        os << "     |\n";
+    }
+    os << "| ------------------------------------------------------------------------------------- |";
+    std::cout << std::endl;
+}
+
+// Explicit instantiations of Collection template class for various types and capacities
+template class Collection<int, 10>;
+template class Collection<double, 10>;
+template class Collection<Book, 10>;
+template class Collection<int, 72>;
+template class Collection<double, 72>;
+template class Collection<Book, 72>;
 
 } // namespace seneca
 
--- P2/OrderedCollection.h
+++ P2/OrderedCollection.h
@@ -8,9 +8,28 @@ namespace seneca {
 template <typename T>
 class OrderedCollection : public Collection<T, 72> {
 public:
-    bool operator+=(const T& item);
+    bool operator+=(const T& item) {
+        if (this->size() < this->capacity()) {
+            int i = this->size() - 1;
+            while (i >= 0 && this->operator[](i) > item) {
+                this->operator[](i + 1) = this->operator[](i);
+                --i;
+            }
+            this->operator[](i + 1) = item;
+            this->incrSize();
+            this->setSmallestItem(item);
+            this->setLargestItem(item);
+            return true;
+        }
+        return false;
+    }
 };
+
+// Explicit instantiations of OrderedCollection template class for various types
+template class OrderedCollection<int>;
+template class OrderedCollection<double>;
+template class OrderedCollection<seneca::Book>;
 
 } // namespace seneca
 
-#endif
+#endif // SENECA_ORDEREDCOLLECTION_H
